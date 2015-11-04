d65db9d014c3add8e5aaf96b3a2977cd2feb19ec
Blake Hagen <blakehagen@gmail.com>
2015-11-03 21:21:21 -0700

    EPL Table on view

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -3,7 +3,7 @@ angular.module('futbolApp').controller('mainCtrl', function($scope, mainService)
     $scope.getData = function(){
         mainService.getPremierLeagueData().then(function(response){
             // console.log(response);
-            $scope.ePL = response;
+            $scope.epl = response;
         })
     }
     
--- js/mainService.js
+++ js/mainService.js
@@ -6,13 +6,21 @@ angular.module('futbolApp').service('mainService', function ($http, $q) {
         var deferred = $q.defer();
         $http({
             method: 'GET',
-            url: 'http://api.football-data.org/alpha/soccerseasons/398/leagueTable',
-            headers: {'Authorization': '57d24f023e8247aea4badd00e37328dc'}
+            url: 'http://api.football-data.org/alpha/soccerseasons/398/leagueTable'
+            // headers: {'Authorization': '57d24f023e8247aea4badd00e37328dc'}
 
         }).then(function (response) {
-            console.log(response.data)
+            console.log(response.data);
 
-            deferred.resolve(response)
+            var epl = response.data.standing;
+            var eplTable = [];
+
+            for (var i = 0; i < epl.length; i++) {
+                eplTable.push({ team: epl[i].teamName });
+            }
+            console.log(eplTable);
+
+            deferred.resolve(eplTable)
         })
         return deferred.promise
 
