c24459370d9177e8730204d40c761d3edf10451a
Blake Hagen <blakehagen@gmail.com>
2015-11-03 17:10:16 -0700

    adding url

--- js/mainService.js
+++ js/mainService.js
@@ -1,5 +1,11 @@
 angular.module('futbolApp').service('mainService', function($http, $q){
     
+    var deferred = $q.defer();
+    $http ({
+        method: 'GET',
+        url: 'http://api.football-data.org/alpha/soccerseasons/405/fixtures'
+    })
+    
     
     
     
