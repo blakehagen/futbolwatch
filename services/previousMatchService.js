9bc6feecfe5dec7cfed4298cd236d5995312e1b1
Blake Hagen <blakehagen@gmail.com>
2015-11-08 21:22:36 -0700

    adding filters for previous matches

--- services/previousMatchService.js
+++ services/previousMatchService.js
@@ -4,7 +4,7 @@ angular.module('waterCoolerFC').service('previousMatchService', function ($http,
         var deferred = $q.defer();
         $http({
             method: 'GET',
-            url: 'http://api.football-data.org/alpha/soccerseasons/' + leagueId + '/fixtures/?timeFrame=p14',
+            url: 'http://api.football-data.org/alpha/soccerseasons/' + leagueId + '/fixtures/?timeFrame=p120',
             headers: { 'X-Auth-Token': '57d24f023e8247aea4badd00e37328dc' }
         }).then(function (response) {
             // console.log(response.data.fixtures);
