8a669eb5c28651972526edacfb8eef99e1efa3c0
Blake Hagen <blakehagen@gmail.com>
2015-11-10 16:44:35 -0700

    removed toggle on each tab

--- controllers/leagueCtrl.js
+++ controllers/leagueCtrl.js
@@ -8,31 +8,28 @@ angular.module('waterCoolerFC').controller('leagueCtrl', function ($scope, idSer
     $scope.id();
     
     // Get League Standings
-    // $scope.toggleStandings = true;
 
     $scope.getStandings = function () {
-            leagueService.getLeagueData($scope.leagueData.leagueId).then(function (response) {
-                $scope.league = response;
-            })
-        
-        $scope.toggleStandings = !$scope.toggleStandings;
+        leagueService.getLeagueData($scope.leagueData.leagueId).then(function (response) {
+            $scope.league = response;
+        })
+
+        $scope.toggleStandings = true;
         $scope.toggleNextMatches = false;
         $scope.togglePreviousMatches = false;
         $scope.toggleScorers = false;
     };
-    
+
     $scope.getStandings();
     
     // Get Upcoming Matches
     $scope.toggleNextMatches = false;
 
     $scope.getNextMatches = function () {
-        if ($scope.toggleNextMatches === false) {
-            upcomingMatchesService.getNextMatches($scope.leagueData.leagueId).then(function (response) {
-                $scope.next = response;
-            })
-        }
-        $scope.toggleNextMatches = !$scope.toggleNextMatches;
+        upcomingMatchesService.getNextMatches($scope.leagueData.leagueId).then(function (response) {
+            $scope.next = response;
+        })
+        $scope.toggleNextMatches = true;
         $scope.toggleStandings = false;
         $scope.togglePreviousMatches = false;
         $scope.toggleScorers = false;
@@ -42,28 +39,23 @@ angular.module('waterCoolerFC').controller('leagueCtrl', function ($scope, idSer
     $scope.togglePreviousMatches = false;
 
     $scope.getPreviousMatches = function () {
-        if ($scope.togglePreviousMatches === false) {
-            previousMatchService.getPreviousMatches($scope.leagueData.leagueId).then(function (response) {
-                $scope.previous = response;
-            })
-        }
-        $scope.togglePreviousMatches = !$scope.togglePreviousMatches;
+        previousMatchService.getPreviousMatches($scope.leagueData.leagueId).then(function (response) {
+            $scope.previous = response;
+        })
+        $scope.togglePreviousMatches = true;
         $scope.toggleScorers = false;
         $scope.toggleStandings = false;
         $scope.toggleNextMatches = false;
-
     };
     
     // Get Top Scorers
     $scope.toggleScorers = false;
 
     $scope.getScorers = function () {
-        if ($scope.toggleScorers === false) {
-            topScorerService.getTopScorers($scope.leagueData.topScorerId).then(function (response) {
-                $scope.scorers = response;
-            })
-        }
-        $scope.toggleScorers = !$scope.toggleScorers;
+        topScorerService.getTopScorers($scope.leagueData.topScorerId).then(function (response) {
+            $scope.scorers = response;
+        })
+        $scope.toggleScorers = true;
         $scope.togglePreviousMatches = false;
         $scope.toggleStandings = false;
         $scope.toggleNextMatches = false;
