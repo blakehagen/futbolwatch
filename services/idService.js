aebd3e68505f2e6ae8a4c2762ddb62f83a98fbc0
Blake Hagen <blakehagen@gmail.com>
2015-11-10 12:12:39 -0700

    routing set up between leagues

--- services/idService.js
+++ services/idService.js
@@ -7,7 +7,9 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
                 leagueLogoPath: 'assets/epl.png',
                 flagPath: 'assets/england.png',
                 leagueHeader: "ENGLISH PREMIER LEAGUE",
-                topScorerId: 'ce15ltey'
+                topScorerId: 'ce15ltey',
+                nextTeam: '#/leagues/spanish-primera',
+                previousTeam: '#/leagues/champions-league'
             }
         } else if ($stateParams.league === 'spanish-primera') {
             return {
@@ -15,7 +17,9 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
                 leagueLogoPath: 'assets/laliga.png',
                 flagPath: 'assets/spain.png',
                 leagueHeader: "SPANISH PRIMERA",
-                topScorerId: 'daaqk8d0'
+                topScorerId: 'daaqk8d0',
+                nextTeam: '#/leagues/bundesliga',
+                previousTeam: '#/leagues/epl'
             }
         } else if ($stateParams.league === 'bundesliga') {
             return {
@@ -23,7 +27,9 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
                 leagueLogoPath: 'assets/bundesliga.png',
                 flagPath: 'assets/germany.png',
                 leagueHeader: "GERMAN BUNDESLIGA",
-                topScorerId: '3vb8ywkg'
+                topScorerId: '3vb8ywkg',
+                nextTeam: '#/leagues/eredivisie',
+                previousTeam: '#/leagues/spanish-primera'
             }
         } else if ($stateParams.league === 'eredivisie') {
             return {
@@ -31,7 +37,9 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
                 leagueLogoPath: 'assets/eredivisie.jpg',
                 flagPath: 'assets/netherlands.png',
                 leagueHeader: "DUTCH EREDIVISIE",
-                topScorerId: 'a71bgaeg'
+                topScorerId: 'a71bgaeg',
+                nextTeam: '#/leagues/serie-a',
+                previousTeam: '#/leagues/bundesliga'
             }
         } else if ($stateParams.league === 'serie-a') {
             return {
@@ -39,7 +47,9 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
                 leagueLogoPath: 'assets/serieA.png',
                 flagPath: 'assets/italy.png',
                 leagueHeader: "ITALY SERIE A",
-                topScorerId: 'dp2cqh4g'
+                topScorerId: 'dp2cqh4g',
+                nextTeam: '#/leagues/portugal-primeira',
+                previousTeam: '#/leagues/eredivisie'
             }
         } else if ($stateParams.league === 'portugal-primeira') {
             return {
@@ -47,7 +57,9 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
                 leagueLogoPath: 'assets/portugalprimeira.png',
                 flagPath: 'assets/portugal.png',
                 leagueHeader: "PORTUGAL PRIMEIRA",
-                topScorerId: 'adx5ccsw'
+                topScorerId: 'adx5ccsw',
+                nextTeam: '#/leagues/ligue1',
+                previousTeam: '#/leagues/serie-a'
             }
         } else if ($stateParams.league === 'ligue1') {
             return {
@@ -55,7 +67,9 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
                 leagueLogoPath: 'assets/ligue1.png',
                 flagPath: 'assets/france.png',
                 leagueHeader: "FRANCE LIGUE 1",
-                topScorerId: '4j8yjehk'
+                topScorerId: '4j8yjehk',
+                nextTeam: '#/leagues/champions-league',
+                previousTeam: '#/leagues/portugal-primeira'
             }
         } else if ($stateParams.league === 'champions-league') {
             return {
@@ -63,7 +77,9 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
                 leagueLogoPath: 'assets/champions-league.png',
                 flagPath: 'assets/uefa.gif',
                 leagueHeader: "UEFA CHAMPIONS LEAGUE",
-                topScorerId: 'dirj8dru'
+                topScorerId: 'dirj8dru',
+                nextTeam: '#/leagues/epl',
+                previousTeam: '#/leagues/ligue1'
             }
         }
     }
