3def1db5e378715aaefd3677abdaa7dad512fc3b
Blake Hagen <blakehagen@gmail.com>
2015-11-12 20:31:49 -0700

    updated

--- services/topScorerService.js
+++ services/topScorerService.js
@@ -4,8 +4,7 @@ angular.module('waterCoolerFC').service('topScorerService', function ($http, $q)
         var deferred = $q.defer();
         $http({
             method: 'GET',
-            url: 'https://www.kimonolabs.com/api/' + id + '?apikey=Gw5PcHFe2RGyDfn2GJlUBynbruB41WWo',
-            headers: {'authorization': 'Bearer SHeFsTtg4WWq3VPnDTPFMDtJqGeDnwbQ'}
+            url: 'https://www.kimonolabs.com/api/' + id + '?apikey=Gw5PcHFe2RGyDfn2GJlUBynbruB41WWo'
         }).then(function (response) {
             var topScorersData = response.data.results.collection;
             // Format Names to not be just initial of first name
