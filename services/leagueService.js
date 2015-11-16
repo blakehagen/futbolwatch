5f120ba2b070885369a12cc3e3eb599e73c1aa9f
Blake Hagen <blakehagen@gmail.com>
2015-11-15 20:14:26 -0700

    new api for league tables

--- services/leagueService.js
+++ services/leagueService.js
@@ -15,7 +15,7 @@ angular.module('waterCoolerFC').service('leagueService', function ($http, $q) {
             // url: 'http://api.football-data.org/alpha/soccerseasons/' + leagueId + '/leagueTable',
             // headers: { 'X-Auth-Token': '57d24f023e8247aea4badd00e37328dc' }
         }).then(function (response) {
-            // console.log(response.data);
+            console.log(response.data);
             var leagueData = response.data.standing;
             var leagueStandings = [];
             for (var i = 0; i < leagueData.length; i++) {
