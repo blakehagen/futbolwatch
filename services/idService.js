051470a8843be074f90604f76c408ad1631d4f1a
Blake Hagen <blakehagen@gmail.com>
2015-11-15 20:12:43 -0700

    new api

--- services/idService.js
+++ services/idService.js
@@ -2,7 +2,8 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
     this.getLeagueId = function () {
         if ($stateParams.league === 'epl') {
             return {
-                leagueId: 398,
+                leagueId: 'dv41vq9a',
+                // leagueId: 398,
                 leagueLogoPath: './assets/optimized/epl-compressor.png',
                 // flagPath: 'assets/england.png',
                 leagueHeader: "ENGLISH PREMIER LEAGUE",
@@ -12,7 +13,8 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
             }
         } else if ($stateParams.league === 'spanish-primera') {
             return {
-                leagueId: 399,
+                leagueId: 'abq5nqls',
+                // leagueId: 399,
                 leagueLogoPath: './assets/optimized/laliga-compressor.png',
                 // flagPath: 'assets/spain.png',
                 leagueHeader: "SPANISH PRIMERA",
@@ -22,7 +24,8 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
             }
         } else if ($stateParams.league === 'bundesliga') {
             return {
-                leagueId: 394,
+                leagueId: '379n3ymy',
+                // leagueId: 394,
                 leagueLogoPath: './assets/optimized/bundesliga-compressor.png',
                 // flagPath: 'assets/germany.png',
                 leagueHeader: "GERMAN BUNDESLIGA",
@@ -32,7 +35,8 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
             }
         } else if ($stateParams.league === 'eredivisie') {
             return {
-                leagueId: 404,
+                leagueId: 'adu2wboi',
+                // leagueId: 404,
                 leagueLogoPath: './assets/optimized/eredivisie-compressor.png',
                 // flagPath: 'assets/netherlands.png',
                 leagueHeader: "DUTCH EREDIVISIE",
@@ -42,7 +46,8 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
             }
         } else if ($stateParams.league === 'serie-a') {
             return {
-                leagueId: 401,
+                leagueId: 'ejr3kwbo',
+                // leagueId: 401,
                 leagueLogoPath: './assets/optimized/serie-a.png',
                 // flagPath: 'assets/italy.png',
                 leagueHeader: "ITALY SERIE A",
@@ -52,7 +57,8 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
             }
         } else if ($stateParams.league === 'portugal-primeira') {
             return {
-                leagueId: 402,
+                leagueId: '40dxlnkq',
+                // leagueId: 402,
                 leagueLogoPath: './assets/optimized/portugal-compressor.png',
                 // flagPath: 'assets/portugal.png',
                 leagueHeader: "PORTUGAL PRIMEIRA",
@@ -62,7 +68,8 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
             }
         } else if ($stateParams.league === 'ligue1') {
             return {
-                leagueId: 396,
+                leagueId: 'a20ppviy',
+                // leagueId: 396,
                 leagueLogoPath: './assets/optimized/ligue1-compressor.png',
                 // flagPath: 'assets/france.png',
                 leagueHeader: "FRANCE LIGUE 1",
@@ -72,7 +79,7 @@ angular.module('waterCoolerFC').service('idService', function ($stateParams) {
             }
         } else if ($stateParams.league === 'champions-league') {
             return {
-                leagueId: 405,
+                // leagueId: 405,
                 leagueLogoPath: './assets/optimized/champions-league-compressor.png',
                 // flagPath: 'assets/uefa./.gif',
                 leagueHeader: "UEFA CHAMPIONS LEAGUE",
