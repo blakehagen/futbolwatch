f2790112643776948763ca5f98b616ec7f08e29d
Blake Hagen <blakehagen@gmail.com>
2015-11-15 20:25:39 -0700

    new api formatted

--- services/leagueService.js
+++ services/leagueService.js
@@ -8,84 +8,79 @@ angular.module('waterCoolerFC').service('leagueService', function ($http, $q) {
         $http({
             method: 'GET',
             url: 'https://www.kimonolabs.com/api/' + leagueId + '?apikey=Gw5PcHFe2RGyDfn2GJlUBynbruB41WWo',
-            
-            
-            
-            
             // url: 'http://api.football-data.org/alpha/soccerseasons/' + leagueId + '/leagueTable',
             // headers: { 'X-Auth-Token': '57d24f023e8247aea4badd00e37328dc' }
         }).then(function (response) {
             console.log(response.data.results.collection1);
-            // var leagueData = response.data.standing;
-            // var leagueStandings = [];
-            // for (var i = 0; i < leagueData.length; i++) {
-            //     if (leagueData[i].teamName === 'Rayo Vallecano de Madrid') {
-            //         leagueData[i].teamName = 'Rayo Vallecano';
-            //     }
-            //     if (leagueData[i].teamName === 'Real Sociedad de Fútbol') {
-            //         leagueData[i].teamName = 'Real Sociedad';
-            //     }
-            //     if (leagueData[i].teamName === 'RCD Espanyol') {
-            //         leagueData[i].teamName = 'Espanyol';
-            //     }
-            //     if (leagueData[i].teamName === 'Olympique Lyonnais') {
-            //         leagueData[i].teamName = 'Olympique Lyon';
-            //     }
-            //     if (leagueData[i].teamName === 'FC Girondins de Bordeaux') {
-            //         leagueData[i].teamName = 'Girondins Bordeaux';
-            //     }
-            //     if (leagueData[i].teamName === 'Olympique de Marseille') {
-            //         leagueData[i].teamName = 'Olympique Marseille';
-            //     }
-            //     if (leagueData[i].teamName === 'Club Atlético de Madrid') {
-            //         leagueData[i].teamName = 'Atlético Madrid';
-            //     }
-            //     if (leagueData[i].teamName === 'RC Deportivo La Coruna') {
-            //         leagueData[i].teamName = 'La Coruña';
-            //     }
-            //     if (leagueData[i].teamName === 'Bor. Mönchengladbach') {
-            //         leagueData[i].teamName = 'M.Gladbach';
-            //     }
-            //     if (leagueData[i].teamName === 'FC Internazionale Milano') {
-            //         leagueData[i].teamName = 'Inter Milano';
-            //     }
-            //     if (leagueData[i].teamName === 'Académica de Coimbra') {
-            //         leagueData[i].teamName = 'Académica Coimbra';
-            //     }
-            //     if (leagueData[i].teamName === 'West Bromwich Albion FC') {
-            //         leagueData[i].teamName = 'West Brom';
-            //     }
-            //     if (leagueData[i].teamName === 'Tottenham Hotspur FC') {
-            //         leagueData[i].teamName = 'Tottenham';
-            //     }
+            var leagueData = response.data.results.collection1;
+            var leagueStandings = [];
+            for (var i = 0; i < leagueData.length; i++) {
+                if (leagueData[i].team.text === 'Rayo Vallecano de Madrid') {
+                    leagueData[i].team.text = 'Rayo Vallecano';
+                }
+                if (leagueData[i].team.text === 'Real Sociedad de Fútbol') {
+                    leagueData[i].team.text = 'Real Sociedad';
+                }
+                if (leagueData[i].team.text === 'RCD Espanyol') {
+                    leagueData[i].team.text = 'Espanyol';
+                }
+                if (leagueData[i].team.text === 'Olympique Lyonnais') {
+                    leagueData[i].team.text = 'Olympique Lyon';
+                }
+                if (leagueData[i].team.text === 'FC Girondins de Bordeaux') {
+                    leagueData[i].team.text = 'Girondins Bordeaux';
+                }
+                if (leagueData[i].team.text === 'Olympique de Marseille') {
+                    leagueData[i].team.text = 'Olympique Marseille';
+                }
+                if (leagueData[i].team.text === 'Club Atlético de Madrid') {
+                    leagueData[i].team.text = 'Atlético Madrid';
+                }
+                if (leagueData[i].team.text === 'RC Deportivo La Coruna') {
+                    leagueData[i].team.text = 'La Coruña';
+                }
+                if (leagueData[i].team.text === 'Bor. Mönchengladbach') {
+                    leagueData[i].team.text = 'M.Gladbach';
+                }
+                if (leagueData[i].team.text === 'FC Internazionale Milano') {
+                    leagueData[i].team.text = 'Inter Milano';
+                }
+                if (leagueData[i].team.text === 'Académica de Coimbra') {
+                    leagueData[i].team.text = 'Académica Coimbra';
+                }
+                if (leagueData[i].team.text === 'West Bromwich Albion FC') {
+                    leagueData[i].team.text = 'West Brom';
+                }
+                if (leagueData[i].team.text === 'Tottenham Hotspur FC') {
+                    leagueData[i].team.text = 'Tottenham';
+                }
 
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('ACF', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('AFC', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('CFC', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('FC', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('CF', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('UD', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('RC', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('TSG', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('SSC', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('BSC', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('SCO', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('OSC', '');
-            //     leagueData[i].teamName = leagueData[i].teamName.replace('SC', '');
-            // }
-            // for (var i = 0; i < leagueData.length; i++) {
-            //     leagueStandings.push({
-            //         league: response.data.leagueCaption,
-            //         team: leagueData[i].teamName.toUpperCase(),
-            //         position: leagueData[i].position,
-            //         points: leagueData[i].points,
-            //         wins: leagueData[i].wins,
-            //         losses: leagueData[i].losses,
-            //         draws: leagueData[i].draws,
-            //         gamesPlayed: leagueData[i].playedGames
-            //     });
-            // }
-            // console.log(leagueStandings);
+                leagueData[i].team.text = leagueData[i].team.text.replace('ACF', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('AFC', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('CFC', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('FC', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('CF', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('UD', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('RC', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('TSG', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('SSC', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('BSC', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('SCO', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('OSC', '');
+                leagueData[i].team.text = leagueData[i].team.text.replace('SC', '');
+            }
+            for (var i = 0; i < leagueData.length; i++) {
+                leagueStandings.push({
+                    team: leagueData[i].team.text.toUpperCase(),
+                    position: leagueData[i].position,
+                    points: leagueData[i].points,
+                    wins: leagueData[i].wins,
+                    losses: leagueData[i].losses,
+                    draws: leagueData[i].draws,
+                    gamesPlayed: leagueData[i].playedGames
+                });
+            }
+            console.log(leagueStandings);
             deferred.resolve(response)
         })
         return deferred.promise
